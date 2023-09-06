f25a984259e5bc3d5a1e014e3dc489e54bff0151
Qinx <me@qinxuzhou.com>
2023-09-05 22:58:02 -0600

    reduce batch size to solve oom error

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import scanpy as sc
 
 
 mcdaa = sc.read_h5ad('data/wang/MCDAA.h5ad')
-TOSICA.train(mcdaa, 'mouse_gobp', project='test', label_name='cell_type')
+TOSICA.train(mcdaa, 'mouse_gobp', project='test', label_name='cell_type', batch_size=4, embed_dim=36)
