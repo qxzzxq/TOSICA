177f88e36d8b0545eda8c75ececdc9be656e7539
Qinx <me@qinxuzhou.com>
2023-09-06 15:44:10 -0600

    change to MCDAA 15k

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import TOSICA
 import scanpy as sc
 
 
-mcdaa = sc.read_h5ad('data/wang/MCDAA.h5ad')
+mcdaa = sc.read_h5ad('data/wang/MCDAA_15k.h5ad')
 TOSICA.train(mcdaa, 'mouse_gobp', project='test', label_name='cell_type', batch_size=4, embed_dim=36)
